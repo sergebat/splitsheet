01b7356ac3c2312efbdd49d7b1c4972d572306e4
Sergey Batishchev <sbatishchev@gmail.com>
2017-02-22 22:40:36 +0400

    Resize target images

--- bin/splitsheet.js
+++ bin/splitsheet.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var shelljs = require("shelljs");
 var jsonfile = require("jsonfile");
-var jimp = require("jimp");
+var Jimp = require("jimp");
 
 
 shelljs.config.verbose = true;
@@ -20,7 +20,7 @@ var frames = json.frames;
 var animations = json.animations;
 var imageFiles = json.images;
 
-jimp.read(imageFiles[0]).then( (originalImage) => {
+Jimp.read(imageFiles[0]).then( (originalImage) => {
     for (var animationName in animations) {
         var animation = animations[animationName];
         var frameIndex = animation.frames[0];
@@ -29,13 +29,13 @@ jimp.read(imageFiles[0]).then( (originalImage) => {
         var imageNameIndex = frame[4];
         var imageFile = imageFiles[imageNameIndex];
 
-        var x = frame[0], y = frame[1], w = frame[2], h = frame[3];
+        var x = frame[0], y = frame[1], w = frame[2], h = frame[3], xr = Math.floor(frame[5]), yr = Math.floor(frame[6]);
 
-        originalImage.clone().crop(x, y, w, h).write(animationName + ".png");
+        var wTarget = Math.max(w - xr, xr) * 2 + 50;
+        var hTarget = Math.max(h - yr, yr) * 2 + 50;
 
-        console.log("Extracting image ", animationName, " from ", imageFile, " at ", x, y, w, h);
-
-        //shelljs.exec(`magick ${imageFile} -crop ${w}x${h}+${x}+${y} ${animationName}.png`);
+        console.log("Extracting image ", animationName, " from ", imageFile, " at ", x, y, w, h, xr, yr );
+        (new Jimp(wTarget, hTarget)).blit(originalImage, wTarget / 2 - xr, hTarget / 2 - yr, x, y, w, h).write(animationName + ".png");
     }
 }).catch(function (err) {
     console.error(err);
